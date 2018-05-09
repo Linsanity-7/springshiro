f202352d7ca5ac9fff5afc6028c35521dc66e41c
Linsanity-7 <l409506800@163.com>
2018-05-09 11:16:32 +0800

    add config of shiro

---
+++ src/main/java/com/example/shiro/realm/ShiroRealm.java
@@ -0,0 +1,4 @@
+package com.example.shiro.realm;
+
+public class ShiroRealm {
+}
---
+++ src/main/java/com/example/shiro/service/impl/SystemUserServiceImpl.java
@@ -0,0 +1,8 @@
+package com.example.shiro.service.impl;
+
+import com.example.shiro.service.SystemUserService;
+import org.springframework.stereotype.Service;
+
+@Service("userService")
+public class SystemUserServiceImpl implements SystemUserService {
+}
