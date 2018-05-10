97243a2eb711e0f12d51c573e7c58e2b411108fb
coder-Lin <409506800@qq.com>
2018-05-10 22:04:07 +0800

    commit loginController

--- src/main/java/com/example/shiro/controller/loginController.java
+++ src/main/java/com/example/shiro/controller/loginController.java
@@ -1,12 +1,11 @@
 package com.example.shiro.controller;
 
-import com.example.shiro.entities.SysUsers;
-import com.example.shiro.service.SystemUserService;
+import com.example.shiro.realm.service.ShiroRealmUserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.annotation.Resource;
 
@@ -14,12 +13,11 @@ import javax.annotation.Resource;
 @Controller
 public class loginController {
     private static final Logger logger = LoggerFactory.getLogger(loginController.class);
-    @Resource
-    private SystemUserService userService;
+    @Autowired
+    private ShiroRealmUserService shiroRealmUserService;
     @RequestMapping(value = "/login.do")
-    public String login(@RequestParam String username,@RequestParam String password){
-        SysUsers user = userService.findObjectByName(username);
-
+    public String login(String username,String password){
+        shiroRealmUserService.login(username,password);
         return null;
     }
 }
--- src/main/java/com/example/shiro/realm/service/impl/ShiroRealmUserServiceImpl.java
+++ src/main/java/com/example/shiro/realm/service/impl/ShiroRealmUserServiceImpl.java
@@ -1,14 +1,36 @@
 package com.example.shiro.realm.service.impl;
 
 import com.example.shiro.dao.SysUsersMapper;
+import com.example.shiro.exception.ServiceException;
 import com.example.shiro.realm.service.ShiroRealmUserService;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.Resource;
 
 @Service
 public class ShiroRealmUserServiceImpl implements ShiroRealmUserService{
-    @Resource
-    private SysUsersMapper userDao;
 
+    private static final Logger logger = LoggerFactory.getLogger(ShiroRealmUserServiceImpl.class);
+
+    @Override
+    public void login(String username, String password) {
+        Subject subject = SecurityUtils.getSubject();
+        if(subject.isAuthenticated())return;
+        UsernamePasswordToken token = new UsernamePasswordToken(username,password);
+        try{
+            subject.login(token);
+        }catch (IncorrectCredentialsException ic){
+            throw new ServiceException("用户登录验证-->密码错误！");
+        }catch (AuthenticationException ae){
+            logger.debug("用户登录验证-->认证失败！");
+            throw new ServiceException("认证失败！");
+        }
+
+    }
 }
